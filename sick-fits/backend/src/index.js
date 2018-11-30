1c46f641eff5f2523ad5ad04873572bc6fb09176
Trevor Eyre <trevoreyre@gmail.com>
2018-11-30 10:38:59 -0700

    Created GraphQL server. Added resolvers to get and create items.

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,17 @@
-// let's go!
+require('dotenv').config({ path: '.env' })
+const createServer = require('./createServer')
+const db = require('./db')
+
+const server = createServer()
+
+// TODO: Use Express middleware to handle JWT
+// TODO: Use Express middleware to populate current user
+
+server.start({
+  cors: {
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  }
+}, info => {
+  console.log(`Server is running on http://localhost:${info.port}`)
+})
